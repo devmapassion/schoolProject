2eccf6ea97fc97a61c19c59ccfd5a3447733be04
Dibocor <dibocor@gmail.com>
2017-12-25 19:35:41 +0100

    create first version index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+ <!DOCTYPE html>
+<html>
+<head>
+  <title>Test Git</title>
+  <meta charset="utf-8">
+</head>
+<body>
+<h1>
+  Hi
+</h1>
+<?php
+ /*$nom = "Dibocor";
+ echo "Bonjour"." ".$nom;*/
+?>
+</body>
+</html>
